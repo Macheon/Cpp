319a23412083cdb653b2f9aaa5b651f9dd564693
Macheon <30942850+Macheon@users.noreply.github.com>
2018-04-08 15:11:58 +0900

    Update dp_merge.cpp

--- dp_merge.cpp
+++ dp_merge.cpp
@@ -21,8 +21,9 @@ int main(void) {
 	ios_base::sync_with_stdio(false);
 	srand((unsigned int)time(NULL));
 	int i, j;
+	int r_time, r_cases, r_sum;
 	int p_time, cases,sum;
-	double ave;
+	double r_ave, ave;
 	bool time_out, sorted;
 	int le, re, size, r, l, sL, sR, move;
 
@@ -30,9 +31,14 @@ int main(void) {
 	ave = 0;
 	sum = 0;
 
+	r_cases = 0;
+	r_ave = 0;
+	r_sum = 0;
+
 	cout << "Rec. start\n";
 	while (true) {
 		time_out = false;
+		r_time = GetTickCount();
 		arr[0] = 0;
 		inx[0] = 0;
 		//arr, inx 초기화
@@ -45,12 +51,18 @@ int main(void) {
 			swap(&arr[i], &arr[rand() % MAX]);
 			swap(&arr[i], &arr[rand() % MAX]);
 		}
+		r_time = GetTickCount() - r_time;
+		r_sum += r_time;
+		r_cases += 1;
+		r_ave = (double)r_sum / (double)r_cases;
 		cout << "Arr. Maked// ";
+		cout << "Case : " << r_cases << " Time record : " << r_time << "ms, Ave : " << r_ave << "ms\n";
 
 		//start
 		p_time = GetTickCount();
 		move = 0;
-		for (int z = 0; z < MAX;z++) {
+		for(int k=0;k<MAX;k++) {
+			
 			le = inx[move];
 			re = inx[le + 1];
 
@@ -59,7 +71,7 @@ int main(void) {
 			size = re - move + 1;
 
 			l = move;
-			r = le + 1;
+			r = sL;
 
 			for (i = 0; i < size; i++) m[i] = ( r >= sR || (l < sL && arr[l] < arr[r]) ) ? arr[l++] : arr[r++];
 			for (i = move, j = 0; i < sR; i++, j++) {
@@ -68,9 +80,10 @@ int main(void) {
 			};
 			move = (sR < MAX && inx[sR] + 1 < MAX)?sR:0;
 			//if (cases != 0 && GetTickCount() - p_time > ave * 2) { cout << "**Timeout****Timeout****Timeout****Timeout****Timeout**\n"; time_out = true; break; }
-		}{
-		p_time = GetTickCount() - p_time;
+		}
 		//end
+		p_time = GetTickCount() - p_time;
+		cout << "Sort. Ended// ";
 		sorted = true;
 		for (int i = 1; i < MAX; i++) {
 			if (arr[i - 1] > arr[i]) {
@@ -78,8 +91,9 @@ int main(void) {
 				break;
 			}
 		}
-		cout << "( ** sorted ** )  ";
-		if (time_out == false) 
+		if(sorted)cout << "( ** sorted ** )  ";
+		else cout << "( ** ERROR ** )";
+		if (time_out == false) {
 			cases += 1;
 			sum += p_time;
 			ave = (double)sum / (double)cases;
